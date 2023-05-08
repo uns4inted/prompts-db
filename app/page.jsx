d3e0307f95e131d20006086943f6b9c78b906959
Ross <lalanskyl@gmail.com>
2023-05-09 00:05:12 +0300

    Update homepage with some content

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,19 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br />
+        <span className="orange_gradient text-center">AI-Powered Prompts</span>
+      </h1>
+      <p className="desc text-center">
+        PromptsDB is a community-driven AI prompting database. It is a place to
+        discover and share your favorite AI prompts.
+      </p>
+
+      {/* Feed */}
+    </section>
+  );
 };
 
 export default Home;
