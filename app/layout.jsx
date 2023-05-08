ad0d07c883282f084f56bf92e29b6c5202bce36f
Ross <lalanskyl@gmail.com>
2023-05-08 23:10:05 +0300

    Create home page and layout components; Fix import error; Replace assets to subfolder;

---
+++ app/layout.jsx
@@ -0,0 +1,22 @@
+import "@styles/globals.css";
+
+export const metadata = {
+  title: "promptsDB",
+  description: "Discover & share your favorite AI prompts",
+};
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+
+        <main className="app">{children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
