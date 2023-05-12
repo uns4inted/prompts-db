50cf1393089fc031a30144dcb5ee0775d81af35c
Ross <lalanskyl@gmail.com>
2023-05-12 19:04:19 +0300

    Fix the NextJS hydration error

--- app/layout.jsx
+++ app/layout.jsx
@@ -13,9 +13,8 @@ const RootLayout = ({ children }) => {
     <html lang="en">
       <body>
         <Provider>
-          <div className="main">
-            <div className="gradient" />
-          </div>
+          <div className="main">⠀</div>
+          <div className="gradient">⠀</div>
           <main className="app">
             <Nav />
             {children}
