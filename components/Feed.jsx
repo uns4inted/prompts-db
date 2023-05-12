44b5971326cc2fee0b5926414701a18a1cc3e0e8
Ross <lalanskyl@gmail.com>
2023-05-12 20:35:50 +0300

    Implement Prompt Feed Page, PromptCard component and API route for fetching data of all prompts

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,5 +1,59 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import PromptCard from "./PromptCard";
+
+// PromptCardList component is implemented for the feed page only
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className="mt-16 prompt_layout">
+      {data.map((prompt) => {
+        return (
+          <PromptCard
+            key={prompt.id}
+            post={prompt}
+            handleTagClick={handleTagClick}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
 const Feed = () => {
-  return <div>Feed</div>;
+  const [searchText, setSearchText] = useState("");
+  const [posts, setPosts] = useState([]); // promts from server
+
+  const handleSearchChange = (e) => {
+    setSearchText(e.target.value);
+  };
+
+  useEffect(() => {
+    // fetch data from server
+    const fetchPosts = async () => {
+      const res = await fetch("/api/prompt");
+      const data = await res.json();
+      setPosts(data);
+    };
+    fetchPosts();
+  }, []);
+
+  return (
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Search for a tag or a username"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input peer"
+        />
+      </form>
+      <PromptCardList data={posts} handleTagClick={() => {}} />
+    </section>
+  );
 };
 
 export default Feed;
