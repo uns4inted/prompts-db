3d83d6444bf1cff7390993956966b7169541f0fb
Ross <lalanskyl@gmail.com>
2023-05-30 00:39:44 +0300

    Replace useEffect with useSWR hook for dynamic data fetching; Add missing import;

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,6 +1,9 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import useSWR from "swr";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 import PromptCard from "./PromptCard";
 
@@ -24,9 +27,11 @@ const PromptCardList = ({ data, handleTagClick }) => {
 const Feed = () => {
   const [searchText, setSearchText] = useState("");
   const [searchTimerId, setSearchTimerId] = useState(null); // timer for search input handler
-  const [posts, setPosts] = useState([]); // all prompts
+  // const [posts, setPosts] = useState([]); // all prompts
   const [filteredPosts, setFilteredPosts] = useState([]); // filtered promts by [searchText]
-  const [loadingStauts, setLoadingStatus] = useState("loading");
+  // const [loadingStauts, setLoadingStatus] = useState("loading");
+
+  const { data: posts, error } = useSWR("/api/prompt", fetcher);
 
   const handleSearchChange = (e) => {
     clearTimeout(searchTimerId); // clear previous timer
@@ -49,31 +54,31 @@ const Feed = () => {
     window.location.reload();
   };
 
-  useEffect(() => {
-    // fetch data from server
-    const fetchPosts = async () => {
-      // Vercel serverless functions are getting cold when not used for a while.
-      const maxRetries = 3;
-      const retryDelay = 200; // 200ms delay between retries
-      let retries = 0;
+  // useEffect(() => {
+  //   // fetch data from server
+  //   const fetchPosts = async () => {
+  //     // Vercel serverless functions are getting cold when not used for a while.
+  //     const maxRetries = 3;
+  //     const retryDelay = 200; // 200ms delay between retries
+  //     let retries = 0;
 
-      while (retries < maxRetries) {
-        try {
-          const res = await fetch("/api/prompt");
-          const data = await res.json();
-          setPosts(data);
-          setLoadingStatus("loaded");
-          break;
-        } catch (error) {
-          retries++;
-          await new Promise((resolve) => setTimeout(resolve, retryDelay));
-        }
-        if (retries === maxRetries) setLoadingStatus("error"); // if auto retry failed, show button for page reload
-      }
-    };
+  //     while (retries < maxRetries) {
+  //       try {
+  //         const res = await fetch("/api/prompt");
+  //         const data = await res.json();
+  //         setPosts(data);
+  //         setLoadingStatus("loaded");
+  //         break;
+  //       } catch (error) {
+  //         retries++;
+  //         await new Promise((resolve) => setTimeout(resolve, retryDelay));
+  //       }
+  //       if (retries === maxRetries) setLoadingStatus("error"); // if auto retry failed, show button for page reload
+  //     }
+  //   };
 
-    fetchPosts();
-  }, []);
+  //   fetchPosts();
+  // }, []);
 
   const filterPrompts = (searchText) => {
     if (!searchText) return posts;
@@ -88,6 +93,33 @@ const Feed = () => {
     return res;
   };
 
+  if (error) {
+    return (
+      <div className="mt-16 flex flex-col items-center">
+        <h1 className="head_text text-center dark: head_text_dark">
+          <span className="text-3xl">Error on loading</span>
+        </h1>
+        <button
+          type="button"
+          className="mt-4 outline_btn dark: outline_btn_white"
+          onClick={handleReloadPageClick}
+        >
+          Try again
+        </button>
+      </div>
+    );
+  }
+
+  if (!posts) {
+    return (
+      <div className="mt-16">
+        <h3 className="head_text text-center dark: head_text_dark">
+          Loading...
+        </h3>
+      </div>
+    );
+  }
+
   return (
     <section className="feed">
       <form className="relative w-full flex-center">
@@ -100,35 +132,54 @@ const Feed = () => {
           className="search_input peer dark: search_input_dark"
         />
       </form>
-      {loadingStauts === "loading" && (
-        <div className="mt-16">
-          <h3 className="head_text text-center dark: head_text_dark">
-            Loading...
-          </h3>
-        </div>
-      )}
-      {loadingStauts === "error" && (
-        <div className="mt-16 flex flex-col items-center">
-          <h1 className="head_text text-center dark: head_text_dark">
-            <span className="text-3xl">Error on loading</span>
-          </h1>
-          <button
-            type="button"
-            className="mt-4 outline_btn dark: outline_btn_white"
-            onClick={handleReloadPageClick}
-          >
-            Try again
-          </button>
-        </div>
-      )}
-      {loadingStauts === "loaded" && (
-        <PromptCardList
-          data={searchText.length ? filteredPosts : posts}
-          handleTagClick={handleTagClick}
-        />
-      )}
+      <PromptCardList
+        data={searchText.length ? filterPrompts(searchText) : posts}
+        handleTagClick={handleTagClick}
+      />
     </section>
   );
+
+  // return (
+  //   <section className="feed">
+  //     <form className="relative w-full flex-center">
+  //       <input
+  //         type="text"
+  //         placeholder="Search by prompt, tag, or username"
+  //         value={searchText}
+  //         onChange={handleSearchChange}
+  //         required
+  //         className="search_input peer dark: search_input_dark"
+  //       />
+  //     </form>
+  //     {loadingStauts === "loading" && (
+  //       <div className="mt-16">
+  //         <h3 className="head_text text-center dark: head_text_dark">
+  //           Loading...
+  //         </h3>
+  //       </div>
+  //     )}
+  //     {loadingStauts === "error" && (
+  //       <div className="mt-16 flex flex-col items-center">
+  //         <h1 className="head_text text-center dark: head_text_dark">
+  //           <span className="text-3xl">Error on loading</span>
+  //         </h1>
+  //         <button
+  //           type="button"
+  //           className="mt-4 outline_btn dark: outline_btn_white"
+  //           onClick={handleReloadPageClick}
+  //         >
+  //           Try again
+  //         </button>
+  //       </div>
+  //     )}
+  //     {loadingStauts === "loaded" && (
+  //       <PromptCardList
+  //         data={searchText.length ? filteredPosts : posts}
+  //         handleTagClick={handleTagClick}
+  //       />
+  //     )}
+  //   </section>
+  // );
 };
 
 export default Feed;
