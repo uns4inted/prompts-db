f86c4bb4468705db153b61b66f4c87c52c05c73a
Ross <lalanskyl@gmail.com>
2023-05-31 15:09:40 +0300

    Add a unique identifier to the URL to force a cache-busting reload

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -4,18 +4,24 @@ import User from "@models/user";
 
 export const GET = async (req) => {
   try {
-
     await connectToDB();
     const prompts = await Prompt.find().populate({
       path: "creator"
     });
 
-    return new Response(JSON.stringify(prompts), {
+    const response = new Response(JSON.stringify(prompts), {
       status: 200,
-      headers: {
-        'Cache-Control': 's-maxage=0, stale-while-revalidate', // disable caching for dynamic content
-      },
     });
+
+    // Add a unique identifier to the URL to force a cache-busting reload
+    const url = new URL(req.url);
+    url.searchParams.set("t", Date.now());
+    response.headers.set("Cache-Control", "no-cache, no-store, must-revalidate");
+    response.headers.set("Pragma", "no-cache");
+    response.headers.set("Expires", "0");
+    response.headers.set("Location", url.toString());
+
+    return response;
   } catch (error) {
     console.error(`Error fetching prompts from database: ${error.message}`);
     return new Response("Failed to fetch prompts from database", {
